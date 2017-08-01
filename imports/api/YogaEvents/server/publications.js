3ad2b16aacca344c1b167cbeb4c4038aa76a13e7
Zack Whipkey <zack.whipkey@gmail.com>
2017-08-01 10:04:48 -0500

    allow yoga events to be public to all auth'd users

--- imports/api/YogaEvents/server/publications.js
+++ imports/api/YogaEvents/server/publications.js
@@ -3,11 +3,11 @@ import { check } from 'meteor/check';
 import YogaEvents from '../YogaEvents';
 
 Meteor.publish('yogaEvents', function yogaEvents() {
-  return YogaEvents.find({ owner: this.userId });
+  return YogaEvents.find();
 });
 
 // Note: yogaEvents.view is also used when editing an existing yogaEvent.
 Meteor.publish('yogaEvents.view', function yogaEventsView(yogaEventId) {
   check(yogaEventId, String);
-  return YogaEvents.find({ _id: yogaEventId, owner: this.userId });
+  return YogaEvents.find({ _id: yogaEventId});
 });
