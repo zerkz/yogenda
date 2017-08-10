e07d159efa0e19572ec102a78c6f840d6470ce0d
Zack Whipkey <zack.whipkey@gmail.com>
2017-08-10 08:36:48 -0500

    remove some unneeded things, fix bug with example pages being imported after being deleted

--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -29,7 +29,6 @@ import NotFound from '../../pages/NotFound/NotFound';
 import Footer from '../../components/Footer/Footer';
 import Terms from '../../pages/Terms/Terms';
 import Privacy from '../../pages/Privacy/Privacy';
-import ExamplePage from '../../pages/ExamplePage/ExamplePage';
 import SvgPractice from '../../pages/SvgPractice/SvgPractice';
 
 import './App.scss';
@@ -55,9 +54,6 @@ const App = props => (
           <Public path="/logout" component={Logout} {...props} />
           <Route name="recover-password" path="/recover-password" component={RecoverPassword} />
           <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
-          <Route name="terms" path="/terms" component={Terms} />
-          <Route name="privacy" path="/privacy" component={Privacy} />
-          <Route name="examplePage" path="/example-page" component={ExamplePage} />
           <Route name="svg" path="/svg" component={SvgPractice} />
           <Route component={NotFound} />
         </Switch>
